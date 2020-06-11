4ada5838f081dca9f79f5b1fc210c6ae9d2f43fa
officialcybersaint <darkocheykov@gmail.com>
2020-06-11 03:22:42 -0400

    NEW maze reader
     implemented maze reading into 2D array

--- src/io.py
+++ src/io.py
@@ -2,11 +2,75 @@
 
 import sys
 import io
+import array
 
-fp = open("./resources/mediumMaze.txt", "r")
+w = open("./resources/bigMaze.txt", "r")
 
-maze = fp.read()
+wi = w.readline().replace('\n','')
+#print(wi)
+width = len(wi) #read first line from file to get the width of the maze
+#print(width)
+w.close() 
 
-maze[25] = "X"
+fp = open("./resources/bigMaze.txt", "r") 
 
-print(maze)
+maze = fp.read() #read entire maze from new file pointer
+mazeStrip = maze.replace('\n','') #strip the maze of newline chars to get accurate reading of #chars
+mazeLen = len(mazeStrip)
+#print(mazeLen)
+#print(maze)
+fp.close()
+    
+height = int(mazeLen/width) #calculate height of maze by amount of chars divided by width
+print(f"height: {height}, width: {width}, total chars: {mazeLen}")
+
+#implement the maze into a 2D array?
+
+mazeArray = [[0 for x in range(width)] for y in range(height)] #2D array to contain the maze
+#print(mazeArray)
+
+xind = 0
+yind = 0
+for char in maze:
+
+    if(char == '\n'): #increment the row counter and reset col counter
+        yind += 1
+        xind = 0
+        continue #skip this char
+    else:
+        #print(f"[{char},{xind},{yind}]", end='')
+        mazeArray[yind][xind] = char #assign char to [row][col] within 2D array
+        xind+=1
+
+
+for x in range(height): #prints the mazeArray
+    for y in range(width):
+        print("%c " % str(mazeArray[x][y]), end='') #print 2D array looking niceeeeeee
+    print()
+
+
+
+
+# xind = 0
+# yind = 0
+# for char in maze:
+    
+#     if(char == '\n' or (xind == width - 1)):
+#         print("yeet")
+#         xind = 0
+#         yind += 1
+#     else:
+#         mazeArray[xind][yind] = char
+#         xind += 1
+
+#     print(f"[{char}|{xind}|{yind}] ", end='')
+#     mazeArray[xind][yind] = char
+    
+    
+    
+
+# for x in range(width):
+#     for y in range(height):
+        
+
+
